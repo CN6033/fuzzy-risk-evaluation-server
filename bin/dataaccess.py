fe42ae79466d6275ca38b229e296dfd8cff358e6
apprentice1989 <apprentice1989@gmail.com>
2013-04-04 09:55:46 +0800

    修改SQL语句传参方式，采用占位符。

--- bin/dataaccess.py
+++ bin/dataaccess.py
@@ -5,25 +5,29 @@ from database import execute
 def getLevelData(v_id):
 	sql = "SELECT id, level, pid " \
 	+"FROM t_level "  \
-	+ "WHERE v_id = " + str(v_id) 
-	return execute(sql)
+	+ "WHERE v_id = %s"
+	params = (str(v_id))
+	return execute(sql, params)
 
 def getEvals(v_id):
 	sql = "SELECT t1.fid, t1.cid "\
 			+" FROM t_eval_res t1, t_class t2 "\
-			+" WHERE t2.v_id = " + str(v_id) \
+			+" WHERE t2.v_id = %s"\
 			+" AND t1.cid = t2.id ;"
-	return execute(sql)
+	params = (str(v_id))
+	return execute(sql, params)
 
 def getClasses(v_id):
 	sql = "SELECT id, value"\
 			+" FROM t_class"\
-			+" WHERE v_id = " + str(v_id)\
+			+" WHERE v_id = %s"\
 			+" ORDER BY value DESC"
-	return execute(sql)
+	params = (str(v_id))
+	return execute(sql, params)
 
 def getWeightResult(v_id):
 	sql = "SELECT fid1, fid2, value" \
 			+" FROM t_weight_res"\
-			+" WHERE v_id=" + str(v_id)
-	return execute(sql)
+			+" WHERE v_id= %s"
+	params = (str(v_id))
+	return execute(sql, params)
