3dc94cd9b7d7e3387a2d5caf1e5763dc0a10b49e
apprentice1989 <apprentice1989@gmail.com>
2013-04-04 21:39:14 +0800

    Add the function getConf()  to get the main process's configuration.

--- bin/configure.py
+++ bin/configure.py
@@ -1,13 +1,19 @@
 #! /usr/bin/python
 
 import ConfigParser
+import logging
+
+DB_CONF_PATH = '../conf/db.conf'
+MAIN_CONF_PATH = '../conf/main.conf'
 
 def getDbConfig():
-#Get the database connect configure
+	"""
+	Get the database connection configure
+	"""
 	conf = {}
 	try:
 		config = ConfigParser.RawConfigParser()
-		config.read('../conf/db.conf')
+		config.read(DB_CONF_PATH)
 		conf["host"] = config.get('mysqld', 'host')
 		conf["user"] = config.get('mysqld', 'user')
 		conf["passwd"] = config.get('mysqld', 'passwd')
@@ -18,7 +24,21 @@ def getDbConfig():
 		conf["maxcached"] = config.getint('mysqld', 'maxcached')
 
 	except IOError as err:
-		print("IOError: " + str(err))
+		logging.error("Exception happened when reading the db configuration file: conf/db.conf!" + str(err))
 		exit(1)
-		
+
+	return conf
+
+def getConf():
+	"""
+		Get the main process' configuration.
+	"""
+	conf = {}
+	try:
+		config = ConfigParser.RawConfigParser()
+		config.read(MAIN_CONF_PATH)
+		conf["log_file_path"] = config.get('main', 'log_file_path')
+	except IOError as err:
+		logging.error("Exception happened when reading the main configuration file: conf/main.conf!" + str(err))
+		exit(1)
 	return conf
