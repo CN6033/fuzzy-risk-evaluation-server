030c876c71e8d6a8115617b566bc70f364a0ca47
apprentice1989 <apprentice1989@gmail.com>
2013-04-08 09:50:02 +0800

    modify the comments.

--- bin/ahp.py
+++ bin/ahp.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+#This module implements the AHP algorithm.
+
+__author__ = 'apprentice1989@gmail.com (Huang Shitao)'
+
 import numpy as np
 
 def calculateWeight(pairwise = []):
-	"Define vector of weight based on eigenvector and eigenvalues"
+	'''Define vector of weight based on eigenvector and eigenvalues'''
 	eigenvalues, eigenvector = np.linalg.eig(pairwise)
 	maxindex = np.argmax(eigenvalues)
 	eigenvalues = np.float32(eigenvalues)
@@ -14,14 +18,14 @@ def calculateWeight(pairwise = []):
 	return weight, eigenvalues,  eigenvector
 
 def Consistency(weight, eigenvalues):
-    "Calculete Consistency index in accord with Saaty (1977)"
-    RI = [0.00, 0.00, 0.00,0.52,0.90,1.12,1.24,1.32,1.41]     #order of matrix: 0,1,2,3,4,5,6,7,8
+    '''Calculete Consistency index in accord with Saaty (1977)'''
+    RI = [0.00, 0.00, 0.00,0.52,0.90,1.12,1.24,1.32,1.41] #order of matrix: 0,1,2,3,4,5,6,7,8
     order = len(weight)
     CI = (np.max(eigenvalues)-order)/(order-1)
     return CI/RI[order-1]
     
 def example():
-	"example"
+	'''example'''
 	#pairwise = [[1.0,1/2.0,4.0,3.0,3.0],[2.0,1.0,7.0,5.0,5.0],[1/4.0,1/7.0,1.0,1/2.0,1.0/3.0],\
 	#	[1/3.0,1/5.0,2.0,1.0,1.0],[1/3.0,1/5.0,3.0,1.0,1.0]]
 	pairwise = [[1,4.0],[1/4.0,1]]
@@ -29,3 +33,6 @@ def example():
 	consistency = Consistency(weight,eigenvalues)
 	print(weight)
 	print(consistency)
+
+if __name__ == '__main__':
+	example()
